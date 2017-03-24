7ecf6fd8032886a3001dc1dfeab878b50cf6b6e3
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-03-23 20:02:49 -0500

    Added more tests.

--- test/oracle-helper-integration-tests.js
+++ test/oracle-helper-integration-tests.js
@@ -12,160 +12,160 @@ const OracleHelper = require('../lib/oracle-helper');
 
 describe('OracleHelper Integration Tests', () => {
 
-    let testObject;
+  let testObject;
 
-    let configOptions = {
-        user: 'system',
-        password: 'oracle',
-        connectString: 'db:1521/xe.oracle.docker'
-    };
+  let configOptions = {
+    user: 'system',
+    password: 'oracle',
+    connectString: 'db:1521/xe.oracle.docker'
+  };
 
-    function waitForConnectionsToClear(pool) {
-        function waitForPoolToClear(pool, resolve) {
-            if (pool.connectionsInUse === 0) {
-                resolve();
-            } else {
-                setTimeout(() => waitForPoolToClear(pool, resolve), 30);
-            }
-        }
-
-        return new Promise(resolve => waitForPoolToClear(pool, resolve));
+  function waitForConnectionsToClear(pool) {
+    function waitForPoolToClear(pool, resolve) {
+      if (pool.connectionsInUse === 0) {
+        resolve();
+      } else {
+        setTimeout(() => waitForPoolToClear(pool, resolve), 30);
+      }
     }
 
-    beforeEach(() => {
-        testObject = new OracleHelper(configOptions);
-    });
+    return new Promise(resolve => waitForPoolToClear(pool, resolve));
+  }
 
-    afterEach(() => {
-        return testObject.destroyPool();
-    });
+  beforeEach(() => {
+    testObject = new OracleHelper(configOptions);
+  });
 
-    describe('createPool', () => {
-        it('it creates successfully', () => {
-            return testObject.createPool()
-                .then(() => {
-                    testObject.pool.should.not.be.undefined;
-                });
+  afterEach(() => {
+    return testObject.destroyPool();
+  });
+
+  describe('createPool', () => {
+    it('it creates successfully', () => {
+      return testObject.createPool()
+        .then(() => {
+          testObject.pool.should.not.be.undefined;
         });
     });
+  });
 
-    describe('destroyPool', () => {
-        it('it resolves successfully when pool does not exist', () => {
-            return testObject.destroyPool()
-                .then(() => expect(testObject.pool).to.be.undefined);
-        });
-
-        it('it resolves successfully', () => {
-            return testObject.createPool()
-                .then(() => testObject.destroyPool())
-                .then(() => expect(testObject.pool).to.be.null);
-        });
+  describe('destroyPool', () => {
+    it('it resolves successfully when pool does not exist', () => {
+      return testObject.destroyPool()
+        .then(() => expect(testObject.pool).to.be.undefined);
     });
 
-    describe('simpleExecute', () => {
-        describe('success path', () => {
-            it('returns the results of the sql operation when successful', function () {
-                this.timeout(1000 * 20);
-                let sql = `select 'foo' as foo from dual`;
-                let params = [];
-
-                return testObject.simpleExecute(sql, params)
-                    .then((results) => {
-                        results.rows[0].FOO.should.eql('foo');
-                        testObject.pool.connectionsInUse.should.eql(0);
-                    });
-            });
-
-            it('returns the results all the sql operations when successful', function () {
-                this.timeout(1000 * 20);
-                let sql = `select 'foo' as foo from dual`;
-                let params = [];
-                return Promise.all([
-                    testObject.simpleExecute(sql, params),
-                    testObject.simpleExecute(sql, params),
-                    testObject.simpleExecute(sql, params),
-                    testObject.simpleExecute(sql, params),
-                    testObject.simpleExecute(sql, params),
-                ]).then((resultsArray) => {
-                    resultsArray.length.should.eql(5);
-                    resultsArray[0].rows[0].FOO.should.eql('foo');
-                    resultsArray[1].rows[0].FOO.should.eql('foo');
-                    resultsArray[2].rows[0].FOO.should.eql('foo');
-                    resultsArray[3].rows[0].FOO.should.eql('foo');
-                    resultsArray[4].rows[0].FOO.should.eql('foo');
-                    testObject.pool.connectionsInUse.should.eql(0);
-                });
-            });
-
-            it('returns the results all the sql operations when successful', function () {
-                this.timeout(1000 * 20);
-                let sql = `select 'foo' as foo from dual`;
-                let params = [];
-                return testObject.simpleExecute(sql, params)
-                    .then(() => testObject.simpleExecute(sql, params))
-                    .then(() => testObject.simpleExecute(sql, params))
-                    .then(() => testObject.simpleExecute(sql, params))
-                    .then(() => testObject.simpleExecute(sql, params))
-                    .then(() => testObject.simpleExecute(sql, params))
-                    .then(() => testObject.simpleExecute(sql, params))
-                    .then(() => testObject.pool.connectionsInUse.should.eql(0));
-            });
-        });
-
-        describe('error path', () => {
-
-            it('returns an error when the one sql operation failed', function () {
-                this.timeout(1000 * 20);
-                let sql = `select foo as foo from dual`;
-                let params = [];
-
-                return testObject.simpleExecute(sql, params)
-                    .then((any) => {
-                        sinon.assert.fail('unexpected promise resolve');
-                    }).catch((error) => {
-                        error.should.match(/^Error: ORA-00904: "FOO": invalid identifier/);
-                        testObject.pool.connectionsInUse.should.eql(0);
-                    });
-            });
-
-            // @see https://github.com/oracle/node-oracledb/issues/350#issuecomment-237922805 (Promise.all issue)
-            it('returns the first error when any of the one sql operations fails', function () {
-                this.timeout(1000 * 20);
-                let sqlBad = `select foo as foo from dual`;
-                let params = [];
-
-                return Promise.all([
-                    testObject.simpleExecute(sqlBad, params),
-                    testObject.simpleExecute(sqlBad, params),
-                    testObject.simpleExecute(sqlBad, params),
-                    testObject.simpleExecute(sqlBad, params),
-                    testObject.simpleExecute(sqlBad, params),
-                ]).then((any) => {
-                    sinon.assert.fail('unexpected promise resolve');
-                }).catch((firstError) => firstError.should.match(/^Error: ORA-00904: "FOO": invalid identifier/))
-                    .then(() => waitForConnectionsToClear(testObject.pool))
-                    .then(() => testObject.pool.connectionsInUse.should.eql(0));
-            });
-
-            // @see https://github.com/oracle/node-oracledb/issues/350#issuecomment-237922805 (Promise.all issue)
-            it('returns the first error when any of the one sql operations fails', function () {
-                this.timeout(1000 * 20);
-                let sqlGood = `select 'foo' as foo from dual`;
-                let sqlBad = `select foo as foo from dual`;
-                let params = [];
-
-                return Promise.all([
-                    testObject.simpleExecute(sqlGood, params),
-                    testObject.simpleExecute(sqlGood, params),
-                    testObject.simpleExecute(sqlGood, params),
-                    testObject.simpleExecute(sqlBad, params),
-                    testObject.simpleExecute(sqlGood, params)
-                ]).then((any) => {
-                    sinon.assert.fail('unexpected promise resolve');
-                }).catch((firstError) => firstError.should.match(/^Error: ORA-00904: "FOO": invalid identifier/))
-                    .then(() => waitForConnectionsToClear(testObject.pool))
-                    .then(() => testObject.pool.connectionsInUse.should.eql(0));
-            });
-        });
+    it('it destroys the pool and resolves successfully when pool exists', () => {
+      return testObject.createPool()
+        .then(() => testObject.destroyPool())
+        .then(() => expect(testObject.pool).to.be.null);
     });
+  });
+
+  describe('simpleExecute', () => {
+    describe('success path', () => {
+      it('returns the results of the sql operation when successful', function () {
+        this.timeout(1000 * 20);
+        let sql = `select 'foo' as foo from dual`;
+        let params = [];
+
+        return testObject.simpleExecute(sql, params)
+          .then((results) => {
+            results.rows[0].FOO.should.eql('foo');
+            testObject.pool.connectionsInUse.should.eql(0);
+          });
+      });
+
+      it('returns the results all the sql operations when successful', function () {
+        this.timeout(1000 * 20);
+        let sql = `select 'foo' as foo from dual`;
+        let params = [];
+        return Promise.all([
+          testObject.simpleExecute(sql, params),
+          testObject.simpleExecute(sql, params),
+          testObject.simpleExecute(sql, params),
+          testObject.simpleExecute(sql, params),
+          testObject.simpleExecute(sql, params),
+        ]).then((resultsArray) => {
+          resultsArray.length.should.eql(5);
+          resultsArray[0].rows[0].FOO.should.eql('foo');
+          resultsArray[1].rows[0].FOO.should.eql('foo');
+          resultsArray[2].rows[0].FOO.should.eql('foo');
+          resultsArray[3].rows[0].FOO.should.eql('foo');
+          resultsArray[4].rows[0].FOO.should.eql('foo');
+          testObject.pool.connectionsInUse.should.eql(0);
+        });
+      });
+
+      it('returns the results all the sql operations when successful', function () {
+        this.timeout(1000 * 20);
+        let sql = `select 'foo' as foo from dual`;
+        let params = [];
+        return testObject.simpleExecute(sql, params)
+          .then(() => testObject.simpleExecute(sql, params))
+          .then(() => testObject.simpleExecute(sql, params))
+          .then(() => testObject.simpleExecute(sql, params))
+          .then(() => testObject.simpleExecute(sql, params))
+          .then(() => testObject.simpleExecute(sql, params))
+          .then(() => testObject.simpleExecute(sql, params))
+          .then(() => testObject.pool.connectionsInUse.should.eql(0));
+      });
+    });
+
+    describe('error path', () => {
+
+      it('returns an error when the one sql operation failed', function () {
+        this.timeout(1000 * 20);
+        let sql = `select foo as foo from dual`;
+        let params = [];
+
+        return testObject.simpleExecute(sql, params)
+          .then((any) => {
+            sinon.assert.fail('unexpected promise resolve');
+          }).catch((error) => {
+            error.should.match(/^Error: ORA-00904: "FOO": invalid identifier/);
+            testObject.pool.connectionsInUse.should.eql(0);
+          });
+      });
+
+      // @see https://github.com/oracle/node-oracledb/issues/350#issuecomment-237922805 (Promise.all issue)
+      it('returns the first error when any of the one sql operations fails', function () {
+        this.timeout(1000 * 20);
+        let sqlBad = `select foo as foo from dual`;
+        let params = [];
+
+        return Promise.all([
+          testObject.simpleExecute(sqlBad, params),
+          testObject.simpleExecute(sqlBad, params),
+          testObject.simpleExecute(sqlBad, params),
+          testObject.simpleExecute(sqlBad, params),
+          testObject.simpleExecute(sqlBad, params),
+        ]).then((any) => {
+          sinon.assert.fail('unexpected promise resolve');
+        }).catch((firstError) => firstError.should.match(/^Error: ORA-00904: "FOO": invalid identifier/))
+          .then(() => waitForConnectionsToClear(testObject.pool))
+          .then(() => testObject.pool.connectionsInUse.should.eql(0));
+      });
+
+      // @see https://github.com/oracle/node-oracledb/issues/350#issuecomment-237922805 (Promise.all issue)
+      it('returns the first error when any of the one sql operations fails', function () {
+        this.timeout(1000 * 20);
+        let sqlGood = `select 'foo' as foo from dual`;
+        let sqlBad = `select foo as foo from dual`;
+        let params = [];
+
+        return Promise.all([
+          testObject.simpleExecute(sqlGood, params),
+          testObject.simpleExecute(sqlGood, params),
+          testObject.simpleExecute(sqlGood, params),
+          testObject.simpleExecute(sqlBad, params),
+          testObject.simpleExecute(sqlGood, params)
+        ]).then((any) => {
+          sinon.assert.fail('unexpected promise resolve');
+        }).catch((firstError) => firstError.should.match(/^Error: ORA-00904: "FOO": invalid identifier/))
+          .then(() => waitForConnectionsToClear(testObject.pool))
+          .then(() => testObject.pool.connectionsInUse.should.eql(0));
+      });
+    });
+  });
 });
--- test/oracle-helper-tests.js
+++ test/oracle-helper-tests.js
@@ -4,6 +4,7 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const should = chai.should();
+const expect = chai.expect;
 chai.use(sinonChai);
 
 const oracledb = require('oracledb');
@@ -11,171 +12,195 @@ const OracleHelper = require('../lib/oracle-helper');
 
 describe('OracleHelper', () => {
 
-    let testObject;
+  let pool;
+  let connection;
+  let testObject;
 
-    let configOptions = {
-        user: 'user',
-        password: 'password',
-        connectString: 'connect string'
+  let configOptions = {
+    user: 'user',
+    password: 'password',
+    connectString: 'connect string'
+  };
+
+  let sql = 'select foo from dual';
+  let args = ['foo', 'bar'];
+  let options = {options: 'some'};
+  let sandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+
+    pool = {
+      getConnection: sandbox.stub(),
+      release: sandbox.stub()
     };
 
-    let sql = 'select foo from dual';
-    let args = ['foo', 'bar'];
-    let options = {options: 'some'};
-    let sandbox;
+    connection = {
+      execute: sandbox.stub(),
+      release: sandbox.stub()
+    };
 
+    testObject = new OracleHelper(configOptions);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('destroyPool', () => {
+    it('it resolves successfully when pool does not exist', () => {
+      return testObject.destroyPool()
+        .then(() => expect(testObject.pool).to.be.undefined);
+    });
+
+    it('it destroys the pool and resolves successfully when pool exists', () => {
+      pool.release.returns(Promise.resolve());
+
+      return testObject.createPool()
+        .then(() => testObject.destroyPool())
+        .then(() => expect(testObject.pool).to.be.null);
+    });
+  });
+
+  describe('createPool', () => {
     beforeEach(() => {
-        sandbox = sinon.sandbox.create();
-        testObject = new OracleHelper(configOptions);
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
     });
 
-    afterEach(() => {
-        sandbox.restore();
+    it('it creates successfully when pool does not exist', () => {
+      return testObject.createPool()
+        .then(() => testObject.pool.should.eql(pool));
     });
 
-    describe('simpleExecute', () => {
-        let pool;
-        let connection;
-        beforeEach(() => {
-            pool = {getConnection: sinon.stub()};
-            connection = {
-                execute: sinon.stub(),
-                release: sinon.stub()
-            };
-        });
-
-        describe('success path', () => {
-
-            beforeEach(() => {
-                pool = {getConnection: sinon.stub()};
-                connection = {
-                    execute: sinon.stub(),
-                    release: sinon.stub()
-                };
-
-                sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
-                pool.getConnection.returns(Promise.resolve(connection));
-                connection.release.returns(Promise.resolve('whatever'));
-            });
-
-            it('uses sane defaults for bindParams to oracledb.execute', () => {
-                connection.execute.returns(Promise.resolve([{}]));
-
-                return testObject.simpleExecute(sql)
-                    .then((results) => {
-                        const expectedParams = {};
-                        connection.execute.calledWith(sql, expectedParams).should.be.true;
-                    });
-            });
-
-            it('uses sane defaults for options to oracledb.execute', () => {
-                connection.execute.returns(Promise.resolve([{}]));
-
-                return testObject.simpleExecute(sql, args)
-                    .then((results) => {
-                        const expectedOptions = {outFormat: oracledb.OBJECT, autoCommit: true};
-                        connection.execute.calledWith(sql, args, expectedOptions).should.be.true;
-                    });
-            });
-
-            it('returns the results of the sql operation when successful', () => {
-                let expectedResults = [{}];
-                connection.execute.withArgs(sql, args, options).returns(Promise.resolve(expectedResults));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .then((results) => {
-                        expectedResults.should.eql(results);
-                    });
-            });
-
-            it('only creates one connection pool', () => {
-                let expectedResults = [{}];
-                connection.execute.withArgs(sql, args, options).returns(Promise.resolve(expectedResults));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .then(() => testObject.simpleExecute(sql, args, options))
-                    .then(() => testObject.simpleExecute(sql, args, options))
-                    .then((result) => {
-                        oracledb.createPool.callCount.should.eql(1);
-                    });
-            });
-
-        });
-
-        describe('error path', () => {
-            it('returns error when creating the pool fails', () => {
-                let expectedError = 'some error';
-                sandbox.stub(oracledb, 'createPool').returns(Promise.reject(expectedError));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .catch((error) => {
-                        expectedError.should.eql(error)
-                    });
-            });
-
-            it('returns error when creating the connection fails', () => {
-                let expectedError = 'some error';
-
-                sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
-                pool.getConnection.returns(Promise.reject(expectedError));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .catch((error) => {
-                        expectedError.should.eql(error)
-                    });
-            });
-
-            it('returns error when executing the sql fails', () => {
-                let expectedError = 'some error';
-
-                sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
-                pool.getConnection.returns(Promise.resolve(connection));
-                connection.release.returns(Promise.resolve('whatever'));
-                connection.execute.returns(Promise.reject(expectedError));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .catch((error) => {
-                        expectedError.should.eql(error)
-                    });
-            });
-
-            it('releases connection when executing the sql fails', () => {
-                let expectedError = 'some error';
-
-                sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
-                pool.getConnection.returns(Promise.resolve(connection));
-                connection.release.returns(Promise.resolve('whatever'));
-                connection.execute.returns(Promise.reject(expectedError));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .catch((error) => {
-                        connection.release.callCount.should.eql(1);
-                    });
-            });
-
-            it('returns results of sql when releasing connection fails', () => {
-                let expectedResults = [{}];
-
-                sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
-                pool.getConnection.returns(Promise.resolve(connection));
-                connection.execute.withArgs(sql, args, options).returns(Promise.resolve(expectedResults));
-                connection.release.returns(Promise.reject('dropped error'));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .then((results) => results.should.eql(expectedResults));
-            });
-
-            it('returns sql execution error when releasing the connection fails', () => {
-                let expectedError = 'some error';
-
-                sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
-                pool.getConnection.returns(Promise.resolve(connection));
-                connection.execute.withArgs(sql, args, options).returns(Promise.reject(expectedError));
-                connection.release.returns(Promise.reject('dropped error'));
-
-                return testObject.simpleExecute(sql, args, options)
-                    .catch((error) => error.should.eql(expectedError));
-            });
+    it('only creates one connection pool', () => {
+      return testObject.createPool()
+        .then(() => testObject.createPool())
+        .then((result) => {
+          oracledb.createPool.callCount.should.eql(1);
         });
     });
+  });
+
+  describe('simpleExecute', () => {
+    describe('success path', () => {
+
+      beforeEach(() => {
+        pool = {getConnection: sinon.stub()};
+        connection = {
+          execute: sinon.stub(),
+          release: sinon.stub()
+        };
+
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
+        pool.getConnection.returns(Promise.resolve(connection));
+        connection.release.returns(Promise.resolve('whatever'));
+      });
+
+      it('uses sane defaults for bindParams to oracledb.execute', () => {
+        connection.execute.returns(Promise.resolve([{}]));
+
+        return testObject.simpleExecute(sql)
+          .then((results) => {
+            const expectedParams = {};
+            connection.execute.calledWith(sql, expectedParams).should.be.true;
+          });
+      });
+
+      it('uses sane defaults for options to oracledb.execute', () => {
+        connection.execute.returns(Promise.resolve([{}]));
+
+        return testObject.simpleExecute(sql, args)
+          .then((results) => {
+            const expectedOptions = {outFormat: oracledb.OBJECT, autoCommit: true};
+            connection.execute.calledWith(sql, args, expectedOptions).should.be.true;
+          });
+      });
+
+      it('returns the results of the sql operation when successful', () => {
+        let expectedResults = [{}];
+        connection.execute.withArgs(sql, args, options).returns(Promise.resolve(expectedResults));
+
+        return testObject.simpleExecute(sql, args, options)
+          .then((results) => {
+            expectedResults.should.eql(results);
+          });
+      });
+    });
+
+    describe('error path', () => {
+      it('returns error when creating the pool fails', () => {
+        let expectedError = 'some error';
+        sandbox.stub(oracledb, 'createPool').returns(Promise.reject(expectedError));
+
+        return testObject.simpleExecute(sql, args, options)
+          .catch((error) => {
+            expectedError.should.eql(error)
+          });
+      });
+
+      it('returns error when creating the connection fails', () => {
+        let expectedError = 'some error';
+
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
+        pool.getConnection.returns(Promise.reject(expectedError));
+
+        return testObject.simpleExecute(sql, args, options)
+          .catch((error) => {
+            expectedError.should.eql(error)
+          });
+      });
+
+      it('returns error when executing the sql fails', () => {
+        let expectedError = 'some error';
+
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
+        pool.getConnection.returns(Promise.resolve(connection));
+        connection.release.returns(Promise.resolve('whatever'));
+        connection.execute.returns(Promise.reject(expectedError));
+
+        return testObject.simpleExecute(sql, args, options)
+          .catch((error) => {
+            expectedError.should.eql(error)
+          });
+      });
+
+      it('releases connection when executing the sql fails', () => {
+        let expectedError = 'some error';
+
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
+        pool.getConnection.returns(Promise.resolve(connection));
+        connection.release.returns(Promise.resolve('whatever'));
+        connection.execute.returns(Promise.reject(expectedError));
+
+        return testObject.simpleExecute(sql, args, options)
+          .catch((error) => {
+            connection.release.callCount.should.eql(1);
+          });
+      });
+
+      it('returns results of sql when releasing connection fails', () => {
+        let expectedResults = [{}];
+
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
+        pool.getConnection.returns(Promise.resolve(connection));
+        connection.execute.withArgs(sql, args, options).returns(Promise.resolve(expectedResults));
+        connection.release.returns(Promise.reject('dropped error'));
+
+        return testObject.simpleExecute(sql, args, options)
+          .then((results) => results.should.eql(expectedResults));
+      });
+
+      it('returns sql execution error when releasing the connection fails', () => {
+        let expectedError = 'some error';
+
+        sandbox.stub(oracledb, 'createPool').returns(Promise.resolve(pool));
+        pool.getConnection.returns(Promise.resolve(connection));
+        connection.execute.withArgs(sql, args, options).returns(Promise.reject(expectedError));
+        connection.release.returns(Promise.reject('dropped error'));
+
+        return testObject.simpleExecute(sql, args, options)
+          .catch((error) => error.should.eql(expectedError));
+      });
+    });
+  });
 });
